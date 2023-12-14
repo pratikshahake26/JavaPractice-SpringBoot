bdf43c7bd2c4324fba217109eac5d89e2e2c8204
omprakash <omprakash@insigniaconsultancy.com>
2023-12-14 22:36:22 +0530

    Initial commit

---
+++ JavaPractice/src/CountLength.java
@@ -0,0 +1,13 @@
+public class CountLength {
+
+    public static void main(String[] args) {
+        String string = "Pratiksha hake";
+        int count = 0;
+        for(int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) != ' ')
+                count++;
+        }
+        System.out.println("Total number of characters in a string: " + count);
+    }
+}
+
